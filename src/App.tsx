3d57c31d9432cd9db46c9c6d3a628a7ca27e18e3
CarlosWirama <carloswirama@gmail.com>
2020-05-23 14:42:34 +0800

    changed default elements from CRA

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Battle Tent
+      </header>
+      <div>
+        LOGIN
+      </div>
+    </div>
+  );
+}
+
+export default App;
