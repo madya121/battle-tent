3596727f5cca4358727b57b44d8e4b2f735be3d6
CarlosWirama <carloswirama@gmail.com>
2020-06-08 00:01:46 +0800

    added Touchable component

--- src/screens/Battle/steps/ChooseParty/ChooseParty.styled.ts
+++ src/screens/Battle/steps/ChooseParty/ChooseParty.styled.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Touchable } from '../../../../components/basics';
 
 export const TileContainer = styled.div`
   display: grid;
@@ -6,7 +7,7 @@ export const TileContainer = styled.div`
   margin: 16px;
 `;
 
-export const Tile = styled.div<{ choosen?: boolean }>`
+export const Tile = styled(Touchable) <{ choosen?: boolean }>`
   border: 1px solid ${props => props.choosen ? 'yellow' : 'white'};
   margin: 8px;
 `;
